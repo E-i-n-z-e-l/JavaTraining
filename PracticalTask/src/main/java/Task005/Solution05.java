5ccac86dcddbbafaf7bf846e1c8822b46f1101d8
Einzel <einzel0102@mail.ru>
2024-11-23 15:04:22 +0800

    "Даны две строки: needle и haystack. Верните индекс первого вхождения needle в haystack, или -1, если needle не является частью haystack. Добавил новые комментарии к решению задачи. "

--- PracticalTask/src/main/java/Task005/Solution05.java
+++ PracticalTask/src/main/java/Task005/Solution05.java
@@ -20,10 +20,22 @@ public class Solution05 {
         System.out.println(solution(haystack03, needle03));
     }
 
+    /**
+     *
+     * @param haystack
+     * @param needle
+     * @return index
+     */
     public static int solution (String haystack, String needle) {
-        int index = -1;
-        char firstCharacter = needle.charAt(0);
+        int index = -1; // Инициализируем переменную index, которая будет хранить индекс первого вхождения
+                        // `needle` в `haystack`. Начальное значение -1 указывает на то, что вхождение не найдено;
 
+        char firstCharacter = needle.charAt(0); // Создаем переменную в которой сохраняем первый символ needle для начала проверки;
+
+        // i <= haystack.length() - needle.length() - используем для того, чтобы убрать лишние проверки.
+        // Например, есть строка "линейка".  Нам нужно взять из этой строки подстроку "ками", которой в строке нет.
+        // И чтобы нам даже не начинать искать подстроку "ками" в строке "линейка", мы отнимаем длину строки от длины
+        // подстроки, чтобы точно убедиться в том, что искомая подстрока по размеру будет подходить в искомой строке.
         for (int i = 0; i <= haystack.length() - needle.length(); i++) {
             if (haystack.charAt(i) == firstCharacter) {
                 // Метод substring(int beginIndex, int endIndex) в Java используется для извлечения подстроки из строки.
